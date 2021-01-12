8010b79e057b91f269300f307a03ce4e7527c2f4
jihyeon <20181039@sungshin.ac.kr>
2021-01-12 22:28:02 +0900

    [FIX] 이름 바꿈

--- Node.js/index.js
+++
@@ -1,5 +0,0 @@
-const myvar = require('./myvar'); //모듈 가저오기
-const setVar = new myvar.Myvar();
-
-console.log(myvar.b);
-console.log(setVar.name);
--- Node.js/myvar.js
+++
@@ -1,8 +0,0 @@
-const a = "hello a";
-
-function Myvar(){
-    this.name = "myvar instance";
-}
-
-module.exports.b = a; //모듈 내보내기
-module.exports.Myvar = Myvar;
