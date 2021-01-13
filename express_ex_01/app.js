3f4ec517f1d2d52057255dcffd935d37944f3d20
jihyeon <20181039@sungshin.ac.kr>
2021-01-13 22:38:16 +0900

    [ADD] express - Router 객체로 라우팅 분리

--- express_ex_01/app.js
+++ express_ex_01/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const admin = require('./routes/admin');
+const contacts = require('./routes/contacts');
+
 const app = express();
 const port = 3000;
 
+//이렇게 경로를 설정하면 경로가 많아졌을 때 비효율적임 
 app.get('/', (req, res)=>{
     res.send('hello express');
 });
@@ -10,6 +14,10 @@ app.get('/', (req, res)=>{
 app.get('/class', (req, res)=>{
     res.send('welcome our class!!');
 });
+
+//따라서 이런 식으로 구현함
+app.use('/contacts', contacts);
+app.use('/admin', admin);
 
 app.listen( port , () => {
     console.log('Express listening on port', port);
