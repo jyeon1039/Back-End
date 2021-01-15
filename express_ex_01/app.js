cc5fd7ea1004f318a246fe1d9241f6b3e9ee2054
jihyeon <20181039@sungshin.ac.kr>
2021-01-15 22:13:37 +0900

    [ADD] form ( body-parser )

--- express_ex_01/app.js
+++ express_ex_01/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks'); //template engine 사용
+const logger = require('morgan');
+const bodyParser = require('body-parser'); //express의 내장 모듈이기 때문에 npm install 안 해줘도 됨
 
 const admin = require('./routes/admin');
 const contacts = require('./routes/contacts');
@@ -16,6 +18,18 @@ nunjucks.configure('template', {
     autoescape : true,
     express : app
 }); 
+
+//미들웨어 셋팅
+app.use( logger('dev') );
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+// body-parser 만들기
+/*app.use( (req, res, next) => {
+    req.body = {
+        //동작
+    }
+});*/
+
 //이렇게 경로를 설정하면 경로가 많아졌을 때 비효율적임 
 app.get('/', (req, res)=>{
     res.send('hello express');
@@ -25,9 +39,14 @@ app.get('/class', (req, res)=>{
     res.send('welcome our class!!');
 });
 
-//따라서 이런 식으로 구현함
-app.use('/contacts', contacts);
-app.use('/admin', admin);
+function vipMiddleware(req, res, next){
+    console.log('최우선 미들웨어');
+    next();
+}
+
+//라우팅을 이런 식으로 구현함
+//app.use('/contacts', contacts);
+app.use('/admin', vipMiddleware, admin);
 
 app.listen( port , () => {
     console.log('Express listening on port', port);
--- express_ex_01/routes/admin.js
+++ express_ex_01/routes/admin.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+function testMiddleWare( res, res, next ){
+    console.log('첫 번째 미들웨어');
+    next();
+}
+
+function testMiddleWare2( res, res, next ){
+    console.log('두 번째 미들웨어');
+    next();
+}
+
+//admin 이후 url들은 /를 보고, 미들웨어 2개를 하나씩 거치고, req, res를 한다
+router.get('/', testMiddleWare, testMiddleWare2, (req, res) => {
     res.send('admin 이후 url');
 });
 
@@ -11,6 +22,15 @@ router.get('/products', (req, res) => {
         message : '<h1> 태그가 출력됩니다. </h1>',
         online : 'express'
     })
+});
+
+router.get('/products/write', (req, res) => {
+    //res.send('admin products url');
+    res.render('admin/write.html');
+});
+
+router.post('/products/write', (req, res) => {
+    res.send(req.body.name + req.body.price);
 });
 
 module.exports = router;
