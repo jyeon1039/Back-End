f40df048656267b5f310bc832dec88b70a429609
jihyeon <20181039@sungshin.ac.kr>
2021-01-15 21:26:23 +0900

    [ADD] 템플릿 엔진과 템플릿 상속

--- express_ex_01/app.js
+++ express_ex_01/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nunjucks = require('nunjucks'); //template engine 사용
 
 const admin = require('./routes/admin');
 const contacts = require('./routes/contacts');
@@ -6,6 +7,15 @@ const contacts = require('./routes/contacts');
 const app = express();
 const port = 3000;
 
+// template 폴더는 'template'이다.
+/* autoescape : 보안상 넣는 것 
+    -> true => html 테그 그대로 출력됨
+    -> false => 렌더링 해 줌
+*/
+nunjucks.configure('template', {
+    autoescape : true,
+    express : app
+}); 
 //이렇게 경로를 설정하면 경로가 많아졌을 때 비효율적임 
 app.get('/', (req, res)=>{
     res.send('hello express');
