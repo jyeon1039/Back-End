2fa60e00404d97fcce0add6377f449ee32b1e1f9
jihyeon <20181039@sungshin.ac.kr>
2021-01-20 20:18:28 +0900

    [ADD] Moment.js 적용, 상세페이지 작성

--- express_ex_01/controllers/admin/admin.ctrl.js
+++ express_ex_01/controllers/admin/admin.ctrl.js
@@ -1,4 +1,5 @@
 const models = require('../../models');
+
 exports.get_products = ( _ , res) => {
     /*res.render( 'admin/products.html' , 
         { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
@@ -18,11 +19,19 @@ exports.get_products_write = ( _ , res) => {
 exports.post_products_write = ( req , res ) => {
     //res.send(req.body);
     console.log(req.body);
+
+    //DB 입력
     models.Products.create({
         name : req.body.name ,
         price : req.body.price ,
         description : req.body.description
     }).then( () => {
         res.redirect('/admin/products');
+    });
+}
+
+exports.get_products_detail = (req, res) => {
+    models.Products.ProductsfindByPk(req.params.id).then( (products) => {
+        res.render( 'admin/detail.html', { product });
     });
 }
