0f7480959a0fdbe29a1fded2c44d5dc13612588f
jihyeon <20181039@sungshin.ac.kr>
2021-01-23 19:48:18 +0900

    [ADD] 업무 자동화 - 작성하기, 수정, 삭제

--- node-puppeteer/template.js
+++ node-puppeteer/template.js
@@ -11,16 +11,56 @@ const modi_description = "update_" + Math.random().toString(36).substring(2, 15)
 async function run (){
 
     // 브라우저 열기
+    // headless => 브라우저가 사용자의 눈에 보여지지 않고 백그라운드 환경에서 작동하는 것
     const browser = await puppeteer.launch();
     const page = await browser.newPage();  
     
 
     // 웹사이트 로딩
-    await page.goto('https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=0&acr=2&acq=%EC%BD%94%EC%8A%A4%ED%94%BC&qdt=0&ie=utf8&query=%EC%BD%94%EC%8A%A4%ED%94%BC',
+    await page.goto('https://localhost:3000/',
      {timeout: 0, waitUntil: 'domcontentloaded'});
 
     const tdName = await page.$eval('.spt_con strong', strong => strong.textContent.trim() );
     console.log(tdName);
+
+    // 작성
+    await page.waitForSelector('.btn-default'); //btn-default 클래스가 로드될 때까지 대기
+    await page.click('.btn-default'); //click을 해야함
+
+    await page.waitForSelector('.btn-primary');
+    
+    await page.evaluate( (a, b) => {
+        document.querySelector('input[name=name]').value = a;
+        document.querySelector('textarea[name=description]').value = b;
+        document.querySelector('.btn-primary').click();
+    }, insert_name, insert_description);
+
+    // 수정
+    await page.waitForSelector('.btn-default');
+    
+    await page.click('table tr:nth-child(2) td:nth-child(1) a');
+
+    await page.waitForSelector('.btn-primary');
+    await page.click('btn-primary');
+
+    await page.waitForSelector('.btn-primary');
+    await page.click('btn-primary');
+
+    await page.evaluate( (a, b) => {
+        document.querySelector('input[name=name]').value = a;
+        document.querySelector('textarea[name=description]').value = b;
+        document.querySelector('.btn-primary').click();
+    }, modi_name, modi_description);
+
+    //"목록으로" 클릭
+    await page.waitForSelector('.btn_default');
+    await page.click('.btn-default');
+    
+    //"작성하기" 클릭
+    await page.waitForSelector('.btn-default');
+
+    //삭제
+    await page.click('.btn-danger');
 
     // 브라우저 닫기
     await browser.close();
